4462f1ed8ff0f1e9715aef5ce22306e4aa41185b
Luiz Henrique de Oliveira <luizhsimionato@gmail.com>
2023-09-04 13:38:09 -0300

    UC7

--- front/src/components/Footer/index.js
+++ front/src/components/Footer/index.js
@@ -17,16 +17,16 @@ function Footer() {
 
         <footer>
                 <div id='comidas'>
-                <Link className='logo' to='/'><img id='banner' src={banner} alt="banner"></img></Link>
+                <img className='comida' src={laranja} alt="laranja"></img>
                 <img className='comida' src={fritas} alt="fritas"></img>
                 <img className='comida' src={pizza} alt="pizza"></img>
                 <img className='comida' src={shawarma} alt="shawarma"></img>
+                <Link className='logo' to='/'><img id='banner' src={banner} alt="banner"></img></Link>
                 <img className='comida' src={pastel} alt="pastel"></img>
                 <img className='comida' src={cachorroquente} alt="cachorroquente"></img>
                 <img className='comida' src={churros} alt="churros"></img>
                 <img className='comida' src={mistoquente} alt="mistoquente"></img>
                 <img className='comida' src={cocacola} alt="cocacola"></img>
-                <img className='comida' src={laranja} alt="laranja"></img>
                 </div>
         </footer>
         
