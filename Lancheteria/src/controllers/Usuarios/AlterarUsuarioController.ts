ac12dbbde807fa5efe306756448f1ab6dd95cbae
Luiz Henrique de Oliveira <luizhsimionato@gmail.com>
2023-09-19 17:21:22 -0300

    UC7

--- Lancheteria/src/controllers/Usuarios/AlterarUsuarioController.ts
+++ Lancheteria/src/controllers/Usuarios/AlterarUsuarioController.ts
@@ -1,4 +1,4 @@
-import { Response, Request } from "express";
+import { Response, Request } from 'express'
 import { AlterarUsuarioServices } from '../../services/Usuarios/AlterarUsuariosServices'
 
 class AlterarUsuarioController{
